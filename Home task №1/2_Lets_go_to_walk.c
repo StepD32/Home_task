8e16bd880a0a7a0ea1112afbf27b20a91b7108eb
StepD32 <zinkinmitya@gmail.com>
2023-12-22 00:23:34 +0300

    Добавление все папки с выполненными заданиями

---
+++ Home task №1/2_Lets_go_to_walk.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+int main(void){
+	printf(" Let's go\n	 to\n	   walk\n");	
+	return 0;
+}
---
+++ Home task №1/3_H_Z.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+int main(void){
+	printf("     *\n    ***\n   *****\n  *******\n   HH HH\n   ZZZZZ\n");	
+	return 0;
+}
