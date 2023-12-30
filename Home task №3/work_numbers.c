c66600f30be8102b86b483536dca333d1e1570f6
StepD32 <zinkinmitya@gmail.com>
2023-12-31 01:57:19 +0300

    Добавлено домашние задание №3

---
+++ Home task №3/work_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+int a,mul;
+	printf("Enter a three-digit number:\n");
+	scanf("%d",&a);
+	mul = a%10;
+	mul *=((a/10)%10);
+	mul *=((a/100)%10);
+ 	printf("A work of a three-digit number = %d\n",mul); 
+	return 0;
+
+}
+
