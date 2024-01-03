e0878777489504aa373e71ec7f7c2d7edd81f9f3
StepD32 <zinkinmitya@gmail.com>
2024-01-03 17:13:46 +0300

    Fix 11 task, triangel

--- Home task №4/11. Тriangle.c
+++ Home task №4/11. Тriangle.c
@@ -6,15 +6,7 @@ int main(int argc, char **argv)
 int a,b,c;	
 	printf("Enter the dimensions of the sides of the triangle\n");
 	scanf("%d %d %d",&a,&b,&c);
-	if (a+b > c)
-	{
-		if (b+c > a)
-		{
-		    if (c+a > b) printf("YES");
-		    else printf("NO");
-		}
-		else printf("NO");					
-	}
+	if ((a+b > c) && (c+a > b) && (b+c > a)) printf("YES");
 	else printf("NO");
 	
 	
