03ff98b797cbd713fb7263d6a413ae3837fd581a
StepD32 <zinkinmitya@gmail.com>
2024-01-14 18:37:46 +0300

    add home task #6

---
+++ Home task №6/C15.c
@@ -0,0 +1,47 @@
+/*
+Цифры по возрастанию
+
+Составить функцию логическую функцию, которая определяет, верно ли, что в заданном числе все цифры стоят по возрастанию. Используя данную функцию решить задачу.
+
+int grow_up(int n)
+
+Данные на входе: 	Одно целое не отрицательное число 
+Данные на выходе: 	YES или NO 
+
+Пример №1
+Данные на входе: 	258 
+Данные на выходе: 	YES 
+
+Пример №2
+Данные на входе: 	528 
+Данные на выходе: 	NO 
+*/
+
+#include <stdio.h>
+
+
+int grow_up(unsigned int a)
+{
+unsigned int other_nums = a;
+    for(int i=0; a!=0;i++)
+    {   
+        int temp_dev = a%10;
+        if (temp_dev < other_nums)        
+            a/=10; 
+        else 
+            return -1;
+        other_nums = temp_dev;
+    }
+    return 0;
+}
+
+int main(void)
+{
+unsigned int a;
+
+    printf("Enter number:\n");
+    scanf("%d",&a);
+    printf("Numbers are in ascending order ");
+    grow_up(a)!=0 ? printf("NO\n"):printf("YES\n");
+    return 0;
+}
---
+++ Home task №6/C17.c
@@ -0,0 +1,49 @@
+/*
+Сумма цифр равна произведению
+
+Составить логическую функцию, которая определяет, верно ли, что в данном числе сумма цифр равна произведению.
+
+int is_happy_number(int n)
+
+Данные на входе: 	Целое не отрицательное число 
+Данные на выходе: 	YES или NO 
+
+Пример №1
+Данные на входе: 	123 
+Данные на выходе: 	YES 
+
+Пример №2
+Данные на входе: 	528 
+Данные на выходе: 	NO 
+
+*/
+
+#include <stdio.h>
+
+
+int is_happy_number(unsigned int a)
+{
+unsigned int sum = 0, mul = 1;
+    for(int i=0; a!=0;i++)
+    {   
+        unsigned int temp_dev = a%10;
+        sum +=temp_dev;
+        mul *=temp_dev;              
+        a/=10;         
+       
+    }
+    return sum == mul ? 0 : 1;
+}
+
+int main(void)
+{
+unsigned int a;
+    printf("Enter number: ");
+    scanf("%d",&a);
+    if (a>0)
+    {
+        printf("The sum of the digits is equal to the product: ");
+        is_happy_number(a)!=0 ? printf("NO\n"):printf("YES\n");
+    }
+    return 0;
+}
