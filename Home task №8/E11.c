ac7507c4a2bbad95e5d0c283248e7d0ac8407fc0
StepD32 <zinkinmitya@gmail.com>
2024-02-04 01:26:33 +0300

    add E13 E15

--- Home task №8/E11.c
+++ Home task №8/E11.c
@@ -14,20 +14,13 @@
 Данные на входе: 	109 118 100 51 62 73 1007 16 4 555 
 Данные на выходе: 	100 51 62 73 4 555 16 1007 118 109 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 871df04adc7a85902ed339668847cc35beb0cec2
 */
 
 #include <stdio.h>
 
 #define  ARR_SIZE 10
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 871df04adc7a85902ed339668847cc35beb0cec2
 int Input (int arr[], int n) //описание функции ввода
 {
 	int i;
@@ -36,49 +29,23 @@ int Input (int arr[], int n) //описание функции ввода
 	return i;
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 871df04adc7a85902ed339668847cc35beb0cec2
 void PrintArray(int arr[], int n)
 {
-	for(int i = 0; i<n; i++){
+	for(int i = 0; i<n; i++)
+	{
 		printf("%d ",arr[i]);
 	}
 }
 
-<<<<<<< HEAD
 
-void shiftArray(int arr[], int size, int shift)
-{
-	int temp[size];
-	for (int i = size-1; i >= 0; i--){
-		if (i+shift >= size)
-			temp[i+shift-size]= arr[i];
-		else
-			temp[i+shift] = arr[i];
-	}
-
-	for (int i = 0; i < size; i++){
-		arr[i] = temp[i];
-	}
-}
-
- 
-int main()
-{
-    int arr[ARR_SIZE];
-	printf("Enter number:\n");
-	Input (arr, ARR_SIZE);
-	PrintArray(arr, ARR_SIZE);
-	return 0;
-} 
-=======
 void SortArray( int arr[], int n)
-{	
-	for (int i = 0; i < n-1; i++){
-		for (int j = (n - 1); j > i; j--){
-			if (arr[j-1]%10 > arr[j]%10){
+{
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = (n - 1); j > i; j--)
+		{
+			if (arr[j-1]%10 > arr[j]%10)
+			{
 				arr[j-1] ^= arr[j];
 				arr[j] ^= arr[j-1];
 				arr[j-1] ^= arr[j];
@@ -88,9 +55,8 @@ void SortArray( int arr[], int n)
 }
 
 
-
 int main()
-{	
+{
 	int arr[ARR_SIZE];
 	printf("Enter number: ");
 	Input (arr, ARR_SIZE);
@@ -99,4 +65,3 @@ int main()
 	return 0;
 }
 
->>>>>>> 871df04adc7a85902ed339668847cc35beb0cec2
