e3ccfa010e57b97381a59ac941e37214f975e887
StepD32 <zinkinmitya@gmail.com>
2024-02-27 02:33:14 +0300

    add task 12

--- Home task 12/temp_api.c
+++ Home task 12/temp_api.c
@@ -23,17 +23,19 @@ uint8_t addRecord (Sensor *date, uint8_t index, uint16_t year, uint8_t mount, ui
     date[index].day = day;
     date[index].hour = hour;
     date[index].min = min;
-    date[index].temperature = temperature;    
+    date[index].temperature = temperature; 
     return index;
 }
 
 int AddInfo(Sensor* info)
 {   
     int num = 0;
-    addRecord(info,num++,2021,12,16,10,50,-99);
-    addRecord(info,num++,2021,8,6,7,40,99);
-    addRecord(info,num++,2021,8,1,7,10,-20);
-    addRecord(info,num++,2021,7,20,10,30,10);
+    addRecord(info,num++,2021,4,10,10,50,3);
+    addRecord(info,num++,2021,8,9,10,50,15);
+    addRecord(info,num++,2021,8,16,10,50,18);
+    addRecord(info,num++,2021,8,6,7,40,35);
+    addRecord(info,num++,2021,8,1,7,10,20);
+    addRecord(info,num++,2021,12,20,10,30,-20);
     return num;
 }            
 
@@ -51,27 +53,27 @@ void print(Sensor *date, int index)
     );
 }
 
-//Высчитываем среднию температуру месяца, проходя по всему массиву структор.
+//Высчитываем среднию температуру в месяцаы, проходя по всему массиву структор.
 int8_t avr_temp_month(Sensor *date, uint8_t size, Season *m)
 {
     int8_t  count = 0;
     int8_t  sum_temp = 0;
     for(int i = 0; i < size; i++){       
        if(date[i].month == m->mount){
-            sum_temp = date[i].temperature;
-             
-            count++;       
-       }
+            sum_temp += date[i].temperature;             
+            count++;
+        }
     }
+    if (!count)
+        return 0;
     return sum_temp/count;
-
 }
 
 //Высчитываем минимальную температуру месяца, проходя по всему массиву структор.
 int8_t min_temp_month(Sensor *date, uint8_t size, Season *m)
 {
-    int min_temp = date[0].temperature;
-    for(int i = 0; i < size; i++){
+    int min_temp = 99;
+    for(int i = 0; i < size; i++) {
        if(date[i].month == m->mount){    
             if (date[i].temperature < min_temp)
                 min_temp = date[i].temperature;
@@ -81,65 +83,91 @@ int8_t min_temp_month(Sensor *date, uint8_t size, Season *m)
 }
 
 //Высчитываем максимальную температуру месяца, проходя по всему массиву структор.
-int max_temp_month(Sensor* date, uint8_t size, Season *m)
+int8_t max_temp_month(Sensor* date, uint8_t size, Season *m)
 {
-    int  max_temp = date[0].temperature;
-    for(uint8_t  i = 0; i < size; i++)
-    {
+    int8_t  max_temp = -99;
+    for(uint8_t  i = 0; i < size; i++){
        if(date[i].month == m->mount){    
             if(date[i].temperature > max_temp)
                 max_temp = date[i].temperature;
        }
     }
-
     return max_temp;
 }
 
-//Высчитываем среднию температуру месяца, проходя по всему массиву структор.
+//Высчитываем среднию температуру в месяцаы, проходя по всему массиву структор.
 int8_t avr_temp_year(Sensor *date, uint8_t size, Season *m)
 {
     int8_t  count = 0;
     int8_t  sum_temp = 0;
-    for(int i = 0; i < size; i++)
-    {       
+    for(int i = 0; i < size; i++){       
        if(date[i].year == m->year){
-            sum_temp = date[i].temperature;
-             
+            sum_temp += date[i].temperature;             
             count++;       
        }
     }
-
+    if (!count)
+        return 0;
     return sum_temp/count;
-
 }
 
 int8_t min_temp_year(Sensor *date, uint8_t size, Season *m)
 {
-
     int min_temp = date[0].temperature;
-    for(int i = 0; i < size; i++)
-    {
-       if(date[i].year == m->year)
-       {    
+    for(int i = 0; i < size; i++){
+       if(date[i].year == m->year){    
             if (date[i].temperature < min_temp)
                 min_temp = date[i].temperature;
        }
     }
-
     return min_temp;
 }
 
 int8_t max_temp_year(Sensor *date, uint8_t size, Season *m)
 {
-
-    int max_temp = date[0].temperature;
-    for(int i = 0; i < size; i++)
-    {
+    int max_temp = 0;
+    for(int i = 0; i < size; i++){
        if(date[i].year == m->year){    
             if (date[i].temperature > max_temp)
                 max_temp = date[i].temperature;
        }
     }
-
     return max_temp;
 }
+
+
+void static_output_fun(Sensor *date, uint8_t size, Season m){
+   //int tmp_index = 0;
+   if (m.mount == Undefined){
+        for(uint8_t i = 1; i <= 13; i++){
+            m.mount = i;
+            for(int j = 0; j<size; j++){
+                if(m.mount == date[j].month && date[j].month != date[j-1].month ){
+                    int8_t avr_month = avr_temp_month(date, size, &m);	
+                    int8_t min_month = min_temp_month(date, size, &m);
+                    int8_t max_month = max_temp_month(date, size, &m);
+                    printf("%d %d %d %d %d %d\n", i, date[j].year, date[j].month, avr_month, min_month, max_month);
+                }
+            }
+        }
+   }else{
+    
+   }
+}
+
+
+
+/*
+void static_output_fun(Sensor *date, uint8_t size, Season m){
+   int tmp_index = 0;
+   if (m.mount == Undefined){
+        for(uint8_t i = 1; i <= 12; i++){
+                int8_t avr_month = avr_temp_month(date, size, &m);	
+                int8_t min_month = min_temp_month(date, size, &m);
+                int8_t max_month = max_temp_month(date, size, &m);
+                printf("%d %d %d %d %d %d\n", i, date[tmp_index].year, date[tmp_index].month, avr_month, min_month, max_month);
+                tmp_index++;
+        }
+   }
+}
+*/
---
+++ Home task 12/temp_api.h
@@ -0,0 +1,76 @@
+#ifndef TEMP_API
+#define TEMP_API
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#define SIZE 30
+
+/*
+Структура типа:
+    - dddd - год 4
+    - цифры mm - месяц 2 символа
+    - dd - день 2 цифры
+    - hh - часы 2 цифры
+    - mm - минуты 2 цифры
+    - temperature - целое число от -99 до 99
+*/
+
+typedef enum {
+    Undefined = 0,
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+
+} Month;
+
+typedef struct {
+    uint16_t year;
+    uint8_t month       :4;
+    uint8_t day         :5;  
+    uint8_t hour        :5;
+    uint8_t min         :6;
+    int8_t temperature;
+    int16_t count;
+} Sensor;
+
+typedef struct{
+    uint16_t year;
+    Month mount;
+} Season;
+
+uint8_t addRecord(Sensor *date, uint8_t index,
+                uint16_t year, 
+                uint8_t mount, 
+                uint8_t day, 
+                uint8_t hour, 
+                uint8_t min, 
+                int8_t temperature            
+            );
+
+int AddInfo(Sensor* info);
+void print(Sensor* date, int num);
+
+int8_t avr_temp_month(Sensor *date, uint8_t size, Season *m);
+int8_t min_temp_month(Sensor *date, uint8_t size, Season *m);
+int8_t max_temp_month(Sensor *date, uint8_t size, Season *m);
+
+int8_t avr_temp_year(Sensor *date, uint8_t size, Season *m);
+int8_t min_temp_year(Sensor *date, uint8_t size, Season *m);
+int8_t max_temp_year(Sensor *date, uint8_t size, Season *m);
+
+void static_output_fun(Sensor *date, uint8_t size, Season m);
+
+
+#endif
